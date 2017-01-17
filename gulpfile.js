4ecded95246597e9f694b1c5154d0a0cb34f07d8
Lytay <lytaytouch@gmail.com>
2017-01-17 10:13:02 +0700

    Add gulp-file package and init node package file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	file = require('gulp-file');
 
-gulp.task('default', function(){
+gulp.task('one', function(){
+	console.log('Task one is triggered!');
+});
+
+gulp.task('two', function(){
+	console.log('Task two is triggered!');
+});
+
+gulp.task('setup', function(){
+	console.log("Setup is actived !");
+});
+gulp.task('default', ['one', 'two'], function(){
 	console.log('Gulp Default Task is triggerd !');
 });
